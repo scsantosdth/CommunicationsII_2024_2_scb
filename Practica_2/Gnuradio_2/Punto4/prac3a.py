5aa83697b6d7d0546a500dd721db8ce6eeb6a419
Sergio Santos <scsantosdth@gmail.com>
2024-10-18 19:21:17 -0500

    Informe 2

--- Practica_2/Gnuradio_2/Punto4/prac3a.py
+++ Practica_2/Gnuradio_2/Punto4/prac3a.py
@@ -78,7 +78,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.h = h = np.array([1,1,1,1])
+        self.h = h = np.array([0.0, 0.5, 1.0, 0.5, 0.0, -0.3, -0.6, -0.3, 0.0])
         self.Sps = Sps = len(h)
         self.Rb = Rb = 32000
         self.samp_rate = samp_rate = Rb*Sps
